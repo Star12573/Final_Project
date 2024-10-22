b1663e7d4efb44b0ebbeeb15d213d7b42ecc6b77
Sierra <sierra.steck@ucas-edu.net>
2024-10-22 20:01:58 +0000

    final

--- final.c
+++ final.c
@@ -1,1 +1,5 @@
-//Game with Sierra
+//Game with Sierra, Cadence, and Addison
+//Should we do hangman?
+//oof...iddk ifme and addi can d 
+//ra
+//I can help when I'm not doing my missing stuff. I mostly only have notes left and then we're good other than the simulator thing
